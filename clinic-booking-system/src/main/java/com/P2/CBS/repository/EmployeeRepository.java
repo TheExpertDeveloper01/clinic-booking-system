6786bd275df8b17924ed08b19ca6b0c83100e60a
TheExpertDeveloper01 <experdev01@proton.me>
2024-11-14 17:51:52 +0000

    Advanced Repository & Business Logic: Custom repo methods
    EmployeeRepository.java enhancements
    
    findByLastName();
    
    findAvailableDoctorsByDate();

--- clinic-booking-system/src/main/java/com/P2/CBS/repository/EmployeeRepository.java
+++ clinic-booking-system/src/main/java/com/P2/CBS/repository/EmployeeRepository.java
@@ -2,7 +2,22 @@ package com.P2.CBS.repository;
 
 import com.P2.CBS.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    // Custom query to find employees by last name
+    List<Employee> findByLastName(String lastName);
+
+    // Find all doctors available on a given date
+    @Query("SELECT e FROM e WHERE e.id NOT IN (SELECT a.doctor.id FROM appointment a WHERE a.date = :date)")
+    List<Employee> findAvailableDoctorsByDate(@Param("date")LocalDate date);
+
+}
