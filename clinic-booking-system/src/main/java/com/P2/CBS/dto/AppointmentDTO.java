36e6458e3d867c718df7577eb6d07918f24f0adb
TheExpertDeveloper01 <experdev01@proton.me>
2024-11-16 19:47:24 +0000

    AppointmentDTO to simplify data being transferred to and from the client instead of passing complete entities.

---
+++ clinic-booking-system/src/main/java/com/P2/CBS/dto/AppointmentDTO.java
@@ -0,0 +1,57 @@
+package com.P2.CBS.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AppointmentDTO {
+
+    private LocalDate date;
+    private LocalTime time;
+    private String status;
+    private Long patientId;
+    private Long doctorId;
+
+
+    // Getters and Setters
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+}
